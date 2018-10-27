561ef069c2400cea4e868ff72791c3d983be310c
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-10-27 23:56:46 +0700

    Project Repository

---
+++ PPMTools-Backend/src/main/java/io/boonkuaeb/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.boonkuaeb.ppmtool.repository;
+
+import io.boonkuaeb.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
