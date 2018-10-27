6cf6b3cd386210c8cd5bb6b90ac326594b19e293
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-10-28 00:27:28 +0700

    validate logic for Create new Project

--- PPMTools-Backend/src/main/java/io/boonkuaeb/ppmtool/domain/Project.java
+++ PPMTools-Backend/src/main/java/io/boonkuaeb/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.boonkuaeb.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,10 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 6, message = "Please use 4 to 6 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
     private Date createdAt;
@@ -86,15 +102,13 @@ public class Project {
     }
 
     @PrePersist
-    protected void onCreate()
-    {
+    protected void onCreate() {
         this.createdAt = new Date();
         this.updatedAt = new Date();
     }
 
     @PreUpdate
-    protected void onUpdate()
-    {
+    protected void onUpdate() {
         this.updatedAt = new Date();
     }
 }
