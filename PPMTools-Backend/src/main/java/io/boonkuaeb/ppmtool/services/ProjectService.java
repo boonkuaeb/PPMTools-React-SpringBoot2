54fa6735cd90eb05bc71cfeb4ec99e391b5f0a1b
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-10-29 09:09:12 +0700

    Find Project by Project code

--- PPMTools-Backend/src/main/java/io/boonkuaeb/ppmtool/services/ProjectService.java
+++ PPMTools-Backend/src/main/java/io/boonkuaeb/ppmtool/services/ProjectService.java
@@ -4,7 +4,6 @@ import io.boonkuaeb.ppmtool.domain.Project;
 import io.boonkuaeb.ppmtool.exceptions.ProjectCodeException;
 import io.boonkuaeb.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,7 +21,16 @@ public class ProjectService {
             project.setProjectCode(project.getProjectCode().toUpperCase());
             return projectRepository.save(project);
         } catch (Exception e) {
-            throw new ProjectCodeException("Project Code '" + project.getProjectCode().toUpperCase() + "' is duplicated");
+            throw new ProjectCodeException("Project Code '" + project.getProjectCode().toUpperCase() + "' already exists");
         }
+    }
+
+    public Project findByProjectCode(String projectCode) {
+        Project project = projectRepository.findByProjectCode(projectCode.toUpperCase());
+        if (project == null) {
+            throw new ProjectCodeException("Project Code '" + projectCode.toUpperCase() + "' doesn't exists");
+        }
+
+        return project;
     }
 }
