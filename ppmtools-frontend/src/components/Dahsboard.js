5ec07175268fc074bc286688f2e53026680628b5
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-10-29 20:59:56 +0700

    first react component

---
+++ ppmtools-frontend/src/components/Dahsboard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Dahsboard extends Component {
+    render() {
+        return (
+            <h1>Welcome to Dashboard</h1>
+        );
+    }
+}
+
+export default Dahsboard;
